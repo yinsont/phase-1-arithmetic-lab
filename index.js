2f4ccd3a73776cb97d7a42b927381c2c8b5b7edc
Yinson Tso <yinson.tso@gmail.com>
2023-03-20 14:13:19 -0400

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random())
+
+let num3 = 34
+let num4 = 5
+
+let mod = num3 % num4
+
+let max = 20
